b9009bd83ae8983ec9adfdc7804df06a78daa0df
Maksym Kaliberda <maxkaliberda1@gmail.com>
2019-07-09 17:47:12 +0300

    fix lint

--- commands/init-stellar.ts
+++ commands/init-stellar.ts
@@ -34,7 +34,7 @@ const asyncForEach = async (array: string[], callback: any) => {
     for (let index = 0; index < array.length; index++) {
       await callback(array[index], index, array);
     }
-}
+};
 
 const FundInternalWallet = async (
         assets: object,
