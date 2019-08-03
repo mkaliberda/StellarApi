32b1dd13955086f5c6a8b0d21bfe36b6b0afc2d7
Maksym Kaliberda <mkaliber@student.unit.ua>
2019-08-03 16:25:19 +0300

    add hold and withdraw test

--- test/e2e/api/StellarAPI.test.ts
+++ test/e2e/api/StellarAPI.test.ts
@@ -27,7 +27,7 @@ describe('apiWallet', () => {
         const response = await request(settings.app).post('/api/wallet/create')
             .set('Accept', 'application/json')
             .send(newAddrParams);
-        firstAddress = JSON.parse(response.text);
+        firstAddress = response.body;
     });
 
     jest.setTimeout(30000);
@@ -71,8 +71,9 @@ describe('apiWallet', () => {
             });
         const balance = await request(settings.app).get(`/api/wallet/balance/${firstAddress}`)
             .query({ 'assets[]': ['DIMO'] });
-        const base = JSON.parse(balance.text).base;
-        expect(base.credit[0].balance).toBe('100.0000000');
+        const base = balance.body.base;
+        console.log(balance.body);
+        // expect(base.credit[0].balance).toBe('100.0000000');
         done();
     });
 
@@ -94,6 +95,73 @@ describe('apiWallet', () => {
             .query({ 'assets[]': ['DIMO'] });
         const balAfterDec =  new Decimal(JSON.parse(balanceProfitAfter.text).base.credit[0].balance);
         expect(new Decimal(fee)).toEqual(balAfterDec.minus(balBeforeDec));
+        done();
+    });
+
+    test('hold-monney-and-unhold-after', async (done) => {
+        // hold-monney to setUp account
+        const amt = 0.99;
+        const astName = 'DIMO';
+        const accountHold = firstAddress;
+        const balanceStart = await request(settings.app).get(`/api/wallet/balance/${ accountHold}`)
+            .query({ 'assets[]': [astName] });
+        const balStartDec =  new Decimal(balanceStart.body.base.credit[0].balance);
+
+        await request(settings.app).post(`/api/wallet/hold/${ accountHold }`)
+            .set('Accept', 'application/json')
+            .send({
+                asset: astName,
+                amount: amt,
+            });
+
+        const balanceAfterHold = await request(settings.app).get(`/api/wallet/balance/${ accountHold }`)
+            .query({ 'assets[]': [astName] });
+        const balAfterHoldDec =  new Decimal(balanceAfterHold.body.base.credit[0].balance);
+        expect(balStartDec.minus(balAfterHoldDec)).toEqual(new Decimal(amt));
+
+        await request(settings.app).post(`/api/wallet/hold/${ accountHold }`)
+            .set('Accept', 'application/json')
+            .send({
+                asset: astName,
+                amount: amt,
+                reverse: true,
+            });
+
+        const balanceAfterReverse  = await request(settings.app).get(`/api/wallet/balance/${ accountHold }`)
+            .query({ 'assets[]': [astName] });
+        const balbalanceAfterReverseDec =  new Decimal(balanceAfterReverse.body.base.credit[0].balance);
+        expect(balbalanceAfterReverseDec.minus(balAfterHoldDec)).toEqual(new Decimal(amt));
+        done();
+    });
+
+    test('hold-monney-and-withdraw-after', async (done) => {
+        // hold-monney to setUp account
+        const amt = 0.99;
+        const astName = 'DIMO';
+        const accountHold = 'CORE_MAIN';
+        const balanceStart = await request(settings.app).get(`/api/wallet/balance/${ accountHold}`)
+            .query({ 'assets[]': [astName] });
+        const balStartDec =  new Decimal(balanceStart.body.base.credit[0].balance);
+
+        await request(settings.app).post(`/api/wallet/hold/${ accountHold }`)
+            .set('Accept', 'application/json')
+            .send({
+                asset: astName,
+                amount: amt,
+            });
+        const balanceAfterHold = await request(settings.app).get(`/api/wallet/balance/${ accountHold }`)
+            .query({ 'assets[]': [astName] });
+        const balAfterHoldDec =  new Decimal(balanceAfterHold.body.base.credit[0].balance);
+        expect(balStartDec.minus(balAfterHoldDec)).toEqual(new Decimal(amt));
+        const resp = await request(settings.app).post(`/api/wallet/hold/${ accountHold }`)
+            .set('Accept', 'application/json')
+            .send({
+                user_acc: accountHold,
+                amount: amt,
+                asset: astName,
+                fee: 12,
+            });
+        console.log('resp!!!!!!', resp.body);
         done();
     });
 
