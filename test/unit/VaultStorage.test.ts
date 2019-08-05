b29b53d700e37f1d16476d45b41f6937491c2e21
Maksym Kaliberda <maxkaliberda1@gmail.com>
2019-08-05 20:10:59 +0300

    fix vault test

--- test/unit/VaultStorage.test.ts
+++ test/unit/VaultStorage.test.ts
@@ -1,6 +1,5 @@
 import { IAccountKeys, IKeysStorage } from '../../src/lib/keys-storage/IStorage';
 import { VaultStorage } from '../../src/lib/keys-storage/VaultStorage';
-import { SYSTEM_ACCOUNTS } from '../../src/lib/stellar/StellarConst';
 
 // // GBSALEPFZQMIAOMLIBKMRGIA2OM4TE265PT2W6BZO6ASFXJGBRI2QKNJ
 
