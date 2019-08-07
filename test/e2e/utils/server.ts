16fa0cddb5e87d5185befa91056250982fbb85f7
Maksym Kaliberda <maxkaliberda1@gmail.com>
2019-08-07 15:30:54 +0300

    clear package command

--- test/e2e/utils/server.ts
+++ test/e2e/utils/server.ts
@@ -1,9 +1,7 @@
-import { setConnection } from 'typeorm-seeding';
 
 import { bootstrapApp } from './bootstrap';
 
 export const prepareServer = async (options?: { migrate: boolean }) => {
     const settings = await bootstrapApp();
-    setConnection(settings.connection);
     return settings;
 };
